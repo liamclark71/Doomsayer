5081c1fae012e1400ec7a537ada029b12c3f8ef5
Jackie Brown <jackie9317@gmail.com>
2026-07-24 17:25:02 -0400

    Blacksmith dialogue

--- point_and_click.gd
+++ point_and_click.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start("timeline")
+	Dialogic.start("village")
 	pass # Replace with function body.
 
 
