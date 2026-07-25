1518a549ca67a2ce4663bd7040540c2e615d6df5
zackochanski <zackochanski@gmail.com>
2026-07-25 01:42:08 -0400

    wave stuff

--- defense.gd
+++ defense.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var wave_announcement = $CanvasLayer/WaveAnnouncement
+
 @export var goblin_scene: PackedScene
 @export var min_spawn_interval: float = 0.5
 @export var spawn_interval: float = 0.01
@@ -8,6 +10,7 @@ extends Node2D
 var spawn_points: Array[Node2D] = []
 var spawn_timer: Timer
 var elapsed_time: float = 0.0
+var wave = 1
 
 
 func _ready():
@@ -17,6 +20,7 @@ func _ready():
 	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
 	add_child(spawn_timer)
 	spawn_timer.start()
+	wave_begin()
 
 
 func _physics_process(delta):
@@ -38,3 +42,13 @@ func _spawn_goblin():
 	var goblin = goblin_scene.instantiate()
 	get_tree().current_scene.add_child(goblin)
 	goblin.global_position = point.global_position
+
+func wave_begin():
+	wave_announcement.get_child(0).get_child(0).text = "Wave %s" % wave
+	wave_announcement.visible = true
+	wave_announcement.modulate = Color.TRANSPARENT
+	var tween = get_tree().create_tween()
+	tween.tween_property(wave_announcement, "modulate", Color(1,1,1,.9), 1)
+	tween.tween_interval(2.0)
+	tween.tween_property(wave_announcement, "modulate", Color.TRANSPARENT, 1)
+	
