fef0d6555bf87bc31b488510da4acae63f8e205c
liamclark71 <liamclarksr71@gmail.com>
2026-07-26 12:13:48 -0400

    fixed pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var resume_button = $Box/MarginContainer/VBoBox/MarginContainer/VBoxContainer/MarginContainer2/ResumeButton
+@onready var resume_button = $Box/MarginContainer/VBoxContainer/MarginContainer2/ResumeButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
